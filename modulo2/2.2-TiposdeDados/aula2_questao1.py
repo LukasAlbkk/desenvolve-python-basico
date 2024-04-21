05722f18cde65cda68a28d316f4b4f940f9a81ee
Lucas Albuquerque <lucasoom3@hotmail.com>
2024-04-21 20:40:12 -0300

    adicao de 2.2-Tipos de Dados

---
+++ modulo2/2.2-TiposdeDados/aula2_questao1.py
@@ -0,0 +1,6 @@
+x = 5
+y = 2
+print(type(x))
+z = x/y
+print(z)
+print(type(z))
---
+++ modulo2/2.2-TiposdeDados/aula2_questao2.py
@@ -0,0 +1,6 @@
+r = "o resultado é"
+x = 10
+y = 3.5
+z = x + y
+print(r,x,y,z)
+print(type(r),type(x),type(y), type(z))
