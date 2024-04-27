457c529584614b5e1ee60cbff46af9e250bbda49
Lucas Albuquerque <lucasoom3@hotmail.com>
2024-04-27 13:46:22 -0300

    5.1_Funcoes

--- modulo5/5.1_Funcoes/aula1_questao5.py
+++ modulo5/5.1_Funcoes/aula1_questao5.py
@@ -1,4 +1,10 @@
 import emoji 
 
-print("Emojis disponíveis: ")
-emoji.emojize()
+print(f"Emojis disponíveis: ")
+print(emoji.emojize(':pink_heart:'))#O red_heart estava saindo pequeno por algum motivo , coloquei o pink
+print(emoji.emojize(':thumbs_up:')) 
+print(emoji.emojize(':thinking_face:'))
+print(emoji.emojize(':partying_face:'))
+
+frase = input("Digite uma frase e ela será emojizada: ")
+print(emoji.emojize(frase))
